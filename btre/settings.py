c69e9427e2a238b824bdc718d216d62fed6951ae
frank <frankeyc777@gmail.com>
2021-01-17 18:10:10 +0800

    Add Models, Do Migration and Modify Admin

--- btre/settings.py
+++ btre/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': 'asdf1234',
+        'HOST': 'localhost'
     }
 }
 
@@ -126,3 +129,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
